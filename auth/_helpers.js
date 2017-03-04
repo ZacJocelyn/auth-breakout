b37d8a7f787dca5afe1da8621b0c61f8f0f63997
ZacJocelyn <zacjocelyn@gmail.com>
2017-03-04 12:21:42 -0700

    added dependecies

---
+++ auth/_helpers.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcrypt');
+const knex = require('../db/knex');
+var localAuth = require('./local')
+
+function createUser(req) {
+  const hash = bcrypt.hashSync(req.body.member.password, 10);
+  return knex('user')
+  .insert({
+    email: req.body.member.email,
+    password: hash,
+  })
+  .returning('*');
+}
+
+
+
+function getUser(email) {
+  return knex('user').where({email}).first();
+}
+
+function comparePass(userPassword, databasePassword) {
+  const bool = bcrypt.compareSync(userPassword, databasePassword);
+  if (!bool) throw new Error('bad password');
+  else return true;
+}
+
+function ensureAuthenticated(req, res, next) {
+  if (!(req.headers && req.headers.authorization)) {
+    return res.status(400).json({
+      status: 'Please log in'
+    });
+  }
+  // decode the token
+  var header = req.headers.authorization.split(' ');
+  var token = header[1];
+  localAuth.decodeToken(token, (err, payload) => {
+    if (err) {
+      return res.status(401).json({
+        status: 'Token has expired'
+      });
+    } else {
+      // check if the user still exists in the db
+      return knex('user').where({id: parseInt(payload.member.id)}).first()
+      .then((user) => {
+        next();
+      })
+      .catch((err) => {
+        res.status(500).json({
+          status: 'error'
+        });
+      });
+    }
+  });
+}
+
+module.exports = {
+  createUser,
+  getUser,
+  comparePass,
+  ensureAuthenticated
+};
